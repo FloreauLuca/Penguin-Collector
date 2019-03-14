84a560150404fed18bba6995a5af945072fc6a5a
Unknown <lucafloreau@akeonet.com>
2019-03-14 16:29:14 +0100

    sprite

---
+++ Penguin Collector/Assets/Script/Noise.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noise : MonoBehaviour
+{
+    private struct Cell
+    {
+        public int x;
+        public int y;
+        public Color color;
+        public bool land;
+    }
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cell[,] grid = new Cell[10, 10];
+        foreach (var cell in grid)
+        {
+            cell.color = Color.Lerp(Color.white, Color.black, Convert.ToSingle(cell.y) / grid.Rank);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
